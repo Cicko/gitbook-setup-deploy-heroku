f5c2fd345f34bdd1bd82b0f14d117b9ff990a105
Rudolf Cicko <rudolfcicko2016@gmail.com>
2017-05-03 22:05:57 +0100

    Performing the gulp

--- files/gulptask.js
+++ files/gulptask.js
@@ -2,14 +2,23 @@
 
 var gulp = require('gulp');
 var exec = require('child_process').exec;
+var fs = require('fs-extra');
+
 gulp.task('deploy-heroku', [], function() {
-  exec('gitbook build',function(err, out){
+  if (!fs.existsSync('_book')) {
+      exec('gitbook build', function (err, out) {
+        updateGit();
+      });
+  }
+  else {
+    updateGit();
+  }
+});
+
+function updateGit () {
+  exec('git add _book',function(err, out){
     if (err) console.log(err);
     console.log(out);
-    exec('git add _book',function(err, out){
-      if (err) console.log(err);
-      console.log(out);
-      exec('git commit -m \"creating book for heroku\"')
-    })
-  })
-});
+    exec('git commit -m \"Updating book for heroku\"')
+  });
+}
