4b42332e5e7dece6fa7d512a0a10ebbeb9d1a3af
Cicko <rudolfcicko2016@gmail.com>
2017-06-04 13:25:28 +0100

    Fixing plugin contamination

--- files/gulptask.js
+++ files/gulptask.js
@@ -1,24 +1,26 @@
 
 
-var gulp = require('gulp');
-var exec = require('child_process').exec;
-var fs = require('fs-extra');
+(function () {
+  var gulp = require('gulp');
+  var exec = require('child_process').exec;
+  var fs = require('fs-extra');
 
-gulp.task('deploy-heroku', [], function() {
-  if (!fs.existsSync('_book')) {
-      exec('gitbook build', function (err, out) {
-        updateGit();
-      });
-  }
-  else {
-    updateGit();
-  }
-});
-
-function updateGit () {
-  exec('git add _book',function(err, out){
-    if (err) console.log(err);
-    console.log(out);
-    exec('git commit -m \"Updating book for heroku\"')
+  gulp.task('deploy-heroku', [], function() {
+    if (!fs.existsSync('_book')) {
+        exec('gitbook build', function (err, out) {
+          updateGit();
+        });
+    }
+    else {
+      updateGit();
+    }
   });
-}
+
+  function updateGit () {
+    exec('git add _book',function(err, out){
+      if (err) console.log(err);
+      console.log(out);
+      exec('git commit -m \"Updating book for heroku\"')
+    });
+  }
+})
