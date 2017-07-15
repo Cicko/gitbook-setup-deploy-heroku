94cb2cbf888102fbfd250d7c068a1fc36163ce81
Cicko <rudolfcicko2016@gmail.com>
2017-07-15 23:23:28 +0100

    Changing all to install it correctly first

--- files/gulptask.js
+++ files/gulptask.js
@@ -2,99 +2,17 @@
 (function () {
   var gulp = require('gulp');
   var exec = require('child_process').exec;
-  var Heroku = require('heroku-client');
-  var heroku;
-  var fs = require('fs-extra');
-
-  var book_json = require('./book.json');
-  var book_name = book_json.title;
-  var author = book_json.author;
-  var heroku_url = book_json.heroku_url;
-
-  var heroku_app_name = book_name + "-" + author + '-gs';
+  var configFile = require('./config.book.json');
 
   gulp.task('deploy-heroku', [], function() {
-    exec ('which heroku', function (err, out) {
-      if (out.length == 0) {
-        console.log("\x1b[31m","YOU HAVE TO INSTALL HEROKU. EXECUTE '$npm install -g heroku'");
-        process.exit();
-      }
-    })
-    if (!fs.existsSync('_book')) {
-        exec('gitbook build', function (err, out) {
-          updateGit();
+      exec('git add .', (err, out) => {
+        if (err) console.log(err);
+        exec('git commit -m \"Updating book for heroku at ' + new Date() + '\"', function (err, out) {
+          exec('git push heroku master', function(err,out) {
+            console.log(out);
+            console.log("Now you can see your document at " + configFile['heroku_url']);
+          })
         });
-    }
-    else {
-      updateGit();
-    }
+      });
   });
-
-  function updateGit () {
-    exec('git add .',function(err, out){
-      if (err) console.log(err);
-      console.log(out);
-      exec('git commit -m \"Updating book for heroku at ' + new Date() + '\"', function (err, out) {
-        exec('git remote -v', function (err, out) {
-          if (out.includes('heroku')) {
-            console.log("heroku remote exists");
-            exec('git push heroku master', function(err,out) {
-              console.log(out);
-              console.log("Now you can see your document at " + book_json['heroku_url']);
-            })
-          }
-          else {
-            console.log("heroku remote don't exists");
-            exec('heroku auth:token', function(err, out) {
-              if (!err) {
-                heroku = new Heroku({
-                  token : out.replace(' ','')
-                });
-                existsHerokuApp(heroku_app_name, function (app) {
-                  if (app) {
-                    setHerokuData(app);
-                  }
-                  else {
-                    heroku.post('/apps', {body: {name: heroku_app_name}}).then(app => {
-                      setHerokuData(app);
-                    }).catch(function(e) {
-                      console.log(e);
-                    });
-                  }
-                });
-              }
-            });
-          }
-        })
-      });
-    });
-  }
-
-  function setHerokuData (app) {
-    book_json['heroku_url'] = app.heroku_url;
-    fs.unlink('./book.json', function(err) {
-      fs.writeFileSync('./book.json', JSON.stringify(book_json, null, '\t'));
-    });
-    exec('git remote add heroku ' + app.git_url, function(err, out) {
-      exec('git push heroku master', function(err, out) {
-        console.log(out);
-        console.log("Now you can see your document at " + app.heroku_url);
-      })
-    });
-  }
-
-
-
-  function existsHerokuApp (name,callback) {
-    var existe = false;
-    heroku.get('/apps').then(apps => {
-      apps.forEach (function (app,i) {
-        console.log(app.name + " and name is: " + name);
-        if (app.name == name) {
-          existe = app;
-        }
-        if (i+1 == apps.length) callback(existe);
-      });
-    });
-  }
 })(this);
--- files/install.js
+++ files/install.js
@@ -2,7 +2,70 @@
 // There the oauth app should be created.
 // There the remote url for the app have to be added to git.
 
+var exec = require('child_process').exec;
+var Heroku = require('heroku-client');
+var heroku;
+var path = require('path')
+var fs = require('fs-extra');
+var configFile = require(path.join(process.cwd(),'config.book.json'));
+var book_name = configFile.title;
+var author = configFile.author;
+var heroku_url = configFile.heroku_url;
+
+var heroku_app_name = book_name + "-" + author + '-gs';
+
+function existsHerokuApp (name,callback) {
+  var existe = false;
+  heroku.get('/apps').then(apps => {
+    apps.forEach (function (app,i) {
+      console.log(app.name + " and name is: " + name);
+      if (app.name == name) {
+        existe = app;
+      }
+      if (i+1 == apps.length) callback(existe);
+    });
+  });
+}
+
+function setHerokuData (app) {
+  configFile['heroku_url'] = app.heroku_url;
+  fs.unlink('./.config.book.json', function(err) {
+    fs.writeFileSync('./.config.book.json', JSON.stringify(configFile, null, '\t'));
+  });
+  exec('git remote add heroku ' + app.git_url, function(err, out) {
+    if (err) console.log(err);
+  });
+}
 
 module.exports.install = (callback) => {
-  callback(null, "install del plugin");
+  exec ('which heroku', function (err, out) {
+    if (out.length == 0) {
+      console.log("\x1b[31m","YOU HAVE TO INSTALL HEROKU. EXECUTE '$npm install -g heroku'");
+      process.exit();
+    }
+  });
+  if (!fs.existsSync('_book')) {
+      exec('gitbook build', function (err, out) {
+        if (err) console.log(err);
+      });
+  }
+  exec('heroku auth:token', function(err, out) {
+    if (!err) {
+      heroku = new Heroku({
+        token : out.replace(' ','')
+      });
+      existsHerokuApp(heroku_app_name, function (app) {
+        if (app) {
+          setHerokuData(app);
+        }
+        else {
+          heroku.post('/apps', {body: {name: heroku_app_name}}).then(app => {
+            setHerokuData(app);
+          }).catch(function(e) {
+            console.log(e);
+          });
+        }
+      });
+    }
+  });
 }
