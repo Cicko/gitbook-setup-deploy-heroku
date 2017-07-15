9af4a7aa087d665c3f48d4a7ebcf2cfea7885713
Cicko <rudolfcicko2016@gmail.com>
2017-07-16 00:17:08 +0100

    Fixing

--- files/install.js
+++ files/install.js
@@ -28,7 +28,6 @@ function existsHerokuApp (name,callback) {
 
 function setHerokuData (app) {
   configFile['heroku_url'] = app.web_url;
-  console.log("Heroku url: " + configFile['heroku_url'])
   fs.unlink('.config.book.json', function(err) {
     fs.writeFileSync('.config.book.json', JSON.stringify(configFile, null, '\t'));
   });
@@ -60,6 +59,7 @@ module.exports.install = (callback) => {
         }
         else {
           heroku.post('/apps', {body: {name: heroku_app_name}}).then(app => {
+            callback("Created heroku app " + heroku_app_name);
             setHerokuData(app);
           }).catch(function(e) {
             console.log(e);
