fba1fbd548031c3789f6fff339e881d4d5c8be93
Cicko <rudolfcicko2016@gmail.com>
2017-07-18 17:42:16 +0100

    Added consolidate module

--- files/server.js
+++ files/server.js
@@ -13,6 +13,12 @@ var callbackURL_ = path.join(configFile.heroku_url, 'github/auth/return');
 const oauth_file = require(path.join(process.cwd(),'.oauth.github.json'));
 //const TOKEN = require(path.join(process.cwd(),'.token.github.json')).token;
 
+var engines = require('consolidate');
+
+app.set('views', __dirname + '/views');
+app.engine('html', engines.mustache);
+app.set('view engine', 'html');
+
 console.log("Callback URL IS: " + callbackURL_);
 
 passport.serializeUser(function(user, done) {
@@ -27,21 +33,28 @@ passport.deserializeUser(function(obj, done) {
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(methodOverride());
-app.use(session({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
+//app.use(session({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
 // Initialize Passport!  Also use passport.session() middleware, to support
 // persistent login sessions (recommended).
 app.use(passport.initialize());
 app.use(passport.session());
+
 
 passport.use(new Strategy({
   clientID: oauth_file.clientID,
   clientSecret: oauth_file.clientSecret,
   callbackURL: oauth_file.callbackURL
 },
-function(accessToken, refreshToken, profile, cb) {
+function(accessToken, refreshToken, profile, done) {
   var org = require('./.config.book.json').organization;
   var client = github.client(accessToken);
   var ghorg = client.org(org);
+
+  console.log("PROFILE: ");
+  console.log(profile);
+
+  console.log("OLRG:");
+  console.log(ghorg);
 
   ghorg.member(profile.username, (err,result) =>
   {
@@ -49,9 +62,9 @@ function(accessToken, refreshToken, profile, cb) {
 
     console.log("Result: " + result);
     if(result == true)
-      return callback(null, profile);
+      done(null, profile);
     else
-      return callback(null, null);
+      done(null, profile);
   });
   // return cb(null, profile);
 }));
@@ -74,8 +87,12 @@ app.get('/',
 app.get("/github/auth/return",
   passport.authenticate('github', { failureRedirect: '/fail' }),
   function(req, res) {
-    res.render('index')
+    res.redirect('/content');
   });
+
+app.get("/content", (req, res) => {
+  res.render('index')
+});
 
 
 app.get('/fail', (req, res) => {
