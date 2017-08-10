fa0fe3013bc7b7f0e905b527f65691feaa9aa34a
Cicko <rudolfcicko2016@gmail.com>
2017-08-10 16:20:57 +0100

    Adding otra vez clipboard

--- files/install.js
+++ files/install.js
@@ -15,6 +15,8 @@ var heroku_app_name = book_name + "-" + author + '-gs';
 var heroku_url = "https://" + heroku_app_name + ".herokuapp.com";
 var launcher = require( 'launch-browser' );
 var Tacks = require('tacks');
+var copy = require('copy-to-clipboard')
+const { URL } = require('url');
 var File = Tacks.File;
 var Dir = Tacks.Dir;
 
@@ -103,14 +105,25 @@ module.exports.install = (callback) => {
     var callback_param_url = '&oauth_application[callback_url]=' + heroku_url + '/github/auth/return';
     var oauth_register_url = base_url + name_param_url + url_param_url + desc_param_url + callback_param_url;
 
+    var canOpenBrowser = false;
+
+    const myURL = new URL(oauth_register_url);
+
     inquirer.prompt([
       {
         type:'input',
         name: 'nothing',
         message: 'Now browser will be opened to create oauth app. YOU HAVE TO COPY the app clientID and clientSecret (Press Enter)',
         filter: function (val) {
-          launcher(oauth_register_url, { browser: ['chromium','chrome', 'firefox', 'safari'] }, function (e, browser) {
-            if(e) return console.log(e);
+          launcher(myURL.href, { browser: ['chromium','chrome', 'firefox', 'safari'] }, function (e, browser) {
+            if(e)  {
+              copy(myURL.href);
+              console.log(e + " Now just CTRL+V on your browser and press Enter. ");
+              //return console.log(e);
+            }
+            else {
+              canOpenBrowser = true;
+            }
             //browser.on('stop', function(code){
             //});
           });
@@ -119,8 +132,23 @@ module.exports.install = (callback) => {
       },
       {
         type: 'input',
+        name: 'justClick',
+        message: 'Press Enter to push client ID:',
+        validate: function (val) {
+          canOpenBrowser = true;
+          return true;
+        },
+        when: function() {
+          return !canOpenBrowser;
+        }
+      },
+      {
+        type: 'input',
         name: 'clientID',
         message: 'Put the client ID of the oauth App: ',
+        when: function() {
+          return canOpenBrowser;
+        },
         validate: function (value) {
           if (!value) return false;
           return true;
